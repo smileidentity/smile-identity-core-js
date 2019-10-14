ade9f523f9dad00d469216d6d304f95e7ffbee3d
Ridhwana <ridhwana.khan16@gmail.com>
2019-10-14 19:11:58 +0200

    feat: add entered to true just in case

--- src/id-api.js
+++ src/id-api.js
@@ -66,11 +66,14 @@ class IDApi {
         return new Signature(_private.data.partner_id, _private.data.api_key).generate_sec_key(timestamp || _private.data.timestamp);
       },
       configureJson: function() {
+        delete _private.data.id_info.entered;
+
         var body =  {
           timestamp: _private.data.timestamp,
           sec_key: _private.determineSecKey().sec_key,
           partner_id: _private.data.partner_id,
           partner_params: _private.data.partner_params,
+          entered: 'true'
         };
 
         return JSON.stringify({...body, ..._private.data.id_info});
