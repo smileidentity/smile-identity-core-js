b61ce3de53e9a99c3f53613ffce545e981cb5132
Dan Bernier <danbernier@gmail.com>
2021-07-14 14:20:42 -0400

    signature.js: fix indentation

--- src/signature.js
+++ src/signature.js
@@ -20,10 +20,10 @@ class Signature {
   confirm_sec_key(timestamp, sec_key) {
     var hash = crypto.createHash('sha256').update(parseInt(this.partnerID, 10) + ":" + timestamp).digest('hex');
     var encrypted = sec_key.split('|')[0];
-  var decrypted = crypto.publicDecrypt({
-    key: Buffer.from(this.apiKey, 'base64'),
-    padding: crypto.constants.RSA_PKCS1_PADDING
-  }, Buffer.from(encrypted, 'base64')).toString();
+    var decrypted = crypto.publicDecrypt({
+      key: Buffer.from(this.apiKey, 'base64'),
+      padding: crypto.constants.RSA_PKCS1_PADDING
+    }, Buffer.from(encrypted, 'base64')).toString();
     return decrypted === hash;
   }
 
