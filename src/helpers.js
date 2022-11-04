b27c5e144e38ee193209c2925dd00f3594d4e81d
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-11-04 12:15:42 -0400

    revert change

--- src/helpers.js
+++ src/helpers.js
@@ -1,30 +1,20 @@
 /**
  * Converts a numeric key to a smile server URI, or
- * returns the URI if it is  parsable.
+ * returns the original URI.
  *
  * @param {string|number} uriOrKey - The URI of a Smile ID server or a
  * numeric key that represents it.
  * @returns {string} URI of smile server if in map, original input if URI.
- * @throws {TypeError} If the input is not a valid URI or key.
  */
 const mapServerUri = (uriOrKey) => {
   const sidServerMapping = {
     0: 'testapi.smileidentity.com/v1',
     1: 'api.smileidentity.com/v1',
   };
-  if (String(uriOrKey) in sidServerMapping) {
+  if (uriOrKey in sidServerMapping) {
     return sidServerMapping[uriOrKey];
   }
-
-  if (typeof uriOrKey === 'string') {
-    // Because server addresses do not have a protocol by default in our class
-    // constructors, we add a https to the beginning of the URI to validate it.
-    // new URL() will throw an error if it is unable to parse the URI.
-    // eslint-disable-next-line no-new
-    new URL(uriOrKey.startsWith('https://') ? uriOrKey : `https://${uriOrKey}`);
-    return uriOrKey;
-  }
-  throw new TypeError('Invalid URL');
+  return uriOrKey;
 };
 
 module.exports = {
--- test/helpers.test.js
+++ test/helpers.test.js
@@ -3,31 +3,22 @@ const { mapServerUri } = require('../src/helpers');
 
 describe('helpers', () => {
   it('mapServerUri', () => {
-    const validTestCases = [
-      { input: 0, expected: 'testapi.smileidentity.com/v1' },
-      { input: 1, expected: 'api.smileidentity.com/v1' },
+    const testCases = [
       { input: '0', expected: 'testapi.smileidentity.com/v1' },
       { input: '1', expected: 'api.smileidentity.com/v1' },
+      { input: 0, expected: 'testapi.smileidentity.com/v1' },
+      { input: 1, expected: 'api.smileidentity.com/v1' },
+      { input: 2, expected: 2 }, // pass through values not in map.
       { input: 'testapi.smileidentity.com/v1', expected: 'testapi.smileidentity.com/v1' },
       { input: 'api.smileidentity.com/v1', expected: 'api.smileidentity.com/v1' },
       { input: 'https://testapi.smileidentity.com/v1', expected: 'https://testapi.smileidentity.com/v1' },
       { input: '10.0.0.1', expected: '10.0.0.1' },
+      { input: undefined, expected: undefined },
+      { input: null, expected: null },
     ];
 
-    validTestCases.forEach((testCase) => {
+    testCases.forEach((testCase) => {
       assert.equal(mapServerUri(testCase.input), testCase.expected);
-    });
-
-    const invalidTestCases = [
-      '',
-      ' ',
-      'https://',
-      null,
-      undefined,
-      2,
-    ];
-    invalidTestCases.forEach((testCase) => {
-      assert.throws(() => mapServerUri(testCase), TypeError);
     });
   });
 });
