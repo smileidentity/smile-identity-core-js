db516f465e1699197e242dff3d5e6b3584f1a073
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-11-04 16:47:54 -0400

    refactor id api class

--- src/id-api.js
+++ src/id-api.js
@@ -1,6 +1,93 @@
 const https = require('https');
 const Signature = require('./signature');
 
+const validatePartnerParams = (partnerParams) => {
+  if (!partnerParams) {
+    throw new Error('Please ensure that you send through partner params');
+  }
+
+  if (typeof partnerParams !== 'object') {
+    throw new Error('Partner params needs to be an object');
+  }
+
+  ['user_id', 'job_id', 'job_type'].forEach((key) => {
+    if (!partnerParams[key]) {
+      throw new Error(`Please make sure that ${key} is included in the partner params`);
+    }
+  });
+
+  if (parseInt(partnerParams.job_type, 10) !== 5) {
+    throw new Error('Please ensure that you are setting your job_type to 5 to query ID Api');
+  }
+};
+
+const validateIdInfo = (idInfo) => {
+  if (typeof idInfo !== 'object') {
+    throw new Error('ID Info needs to be an object');
+  }
+
+  if (!idInfo || Object.keys(idInfo).length === 0) {
+    throw new Error('Please make sure that id_info not empty or nil');
+  }
+
+  if (!idInfo.id_number || idInfo.id_number.length === 0) {
+    throw new Error('Please provide an id_number in the id_info payload');
+  }
+};
+
+const configureJson = (data, options) => {
+  const body = {
+    timestamp: data.timestamp,
+    partner_id: data.partner_id,
+    partner_params: data.partner_params,
+    language: 'javascript',
+  };
+  const signature = new Signature(data.partner_id, data.api_key);
+
+  if (options && options.signature) {
+    body.signature = signature.generate_signature(data.timestamp).signature;
+  } else {
+    body.sec_key = signature.generate_sec_key(data.timestamp).sec_key;
+  }
+  return JSON.stringify({ ...body, ...data.id_info });
+};
+
+const setupRequests = (data, options) => new Promise((resolve, reject) => {
+  let json = '';
+  const path = `/${data.url.split('/')[1]}/id_verification`;
+  const host = data.url.split('/')[0];
+  const body = configureJson(data, options);
+  const reqOptions = {
+    hostname: host,
+    path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const req = https.request(reqOptions, (resp) => {
+    resp.setEncoding('utf8');
+    resp.on('data', (chunk) => {
+      json += chunk;
+    });
+
+    resp.on('end', () => {
+      if (resp.statusCode === 200) {
+        resolve(JSON.parse(json));
+        return;
+      }
+      const err = JSON.parse(json);
+      reject(new Error(`${err.code}:${err.error}`));
+    });
+  });
+  req.write(body);
+  req.end();
+
+  req.on('error', (err) => {
+    reject(new Error(`${err.code}:${err.error}`));
+  });
+});
+
 class IDApi {
   constructor(partner_id, api_key, sid_server) {
     this.partner_id = partner_id;
@@ -19,130 +106,24 @@ class IDApi {
   }
 
   submit_job(partner_params, id_info, options = {}) {
-    const _private = {
-      data: {
-        timestamp: options.signature ? new Date().toISOString() : Date.now(),
-        url: this.url,
-        partner_id: this.partner_id,
-        api_key: this.api_key,
-        sid_server: this.sid_server,
-      },
-      validateInputs() {
-        _private.partnerParams(partner_params);
-        _private.idInfo(id_info);
-      },
-      partnerParams(partnerParams) {
-        if (!partnerParams) {
-          throw new Error('Please ensure that you send through partner params');
-        }
-
-        if (typeof partnerParams !== 'object') {
-          throw new Error('Partner params needs to be an object');
-        }
-
-        ['user_id', 'job_id', 'job_type'].forEach((key) => {
-          if (!partnerParams[key]) {
-            throw new Error(`Please make sure that ${key} is included in the partner params`);
-          }
-        });
-
-        partnerParams.job_type = parseInt(partnerParams.job_type, 10);
-        if (parseInt(partnerParams.job_type, 10) !== 5) {
-          throw new Error('Please ensure that you are setting your job_type to 5 to query ID Api');
-        }
-
-        _private.data.partner_params = partnerParams;
-      },
-      idInfo(idInfo) {
-        if (typeof idInfo !== 'object') {
-          throw new Error('ID Info needs to be an object');
-        }
-
-        if (!idInfo || Object.keys(idInfo).length === 0) {
-          throw new Error('Please make sure that id_info not empty or nil');
-        }
-
-        if (!idInfo.id_number || idInfo.id_number.length === 0) {
-          throw new Error('Please provide an id_number in the id_info payload');
-        }
-
-        _private.data.id_info = idInfo;
-      },
-      determineSecKey() {
-        return new Signature(
-          _private.data.partner_id,
-          _private.data.api_key,
-        ).generate_sec_key(_private.data.timestamp);
-      },
-      determineSignature() {
-        return new Signature(
-          _private.data.partner_id,
-          _private.data.api_key,
-        ).generate_signature(_private.data.timestamp);
-      },
-      configureJson() {
-        const body = {
-          timestamp: _private.data.timestamp,
-          partner_id: _private.data.partner_id,
-          partner_params: _private.data.partner_params,
-          language: 'javascript',
-        };
-        if (options && options.signature) {
-          body.signature = _private.determineSignature().signature;
-        } else {
-          body.sec_key = _private.determineSecKey().sec_key;
-        }
-        return JSON.stringify({ ...body, ..._private.data.id_info });
-      },
-      setupRequests() {
-        let json = '';
-        const path = `/${_private.data.url.split('/')[1]}/id_verification`;
-        const host = _private.data.url.split('/')[0];
-        const body = _private.configureJson();
-        const reqOptions = {
-          hostname: host,
-          path,
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        };
-        const req = https.request(reqOptions, (resp) => {
-          resp.setEncoding('utf8');
-          resp.on('data', (chunk) => {
-            json += chunk;
-          });
-
-          resp.on('end', () => {
-            if (resp.statusCode === 200) {
-              _private.data.resolve(JSON.parse(json));
-              return;
-            }
-            const err = JSON.parse(json);
-            _private.data.reject(new Error(`${err.code}:${err.error}`));
-          });
-        });
-        req.write(body);
-        req.end();
-
-        req.on('error', (err) => {
-          _private.data.reject(`${err.code}:${err.error}`);
-        });
-      },
+    const data = {
+      timestamp: options.signature ? new Date().toISOString() : Date.now(),
+      url: this.url,
+      partner_id: this.partner_id,
+      api_key: this.api_key,
+      sid_server: this.sid_server,
+      partner_params,
+      id_info,
     };
 
-    // this section kicks everything off
-    const result = new Promise((resolve, reject) => {
-      try {
-        _private.data.resolve = resolve;
-        _private.data.reject = reject;
-        _private.validateInputs();
-        _private.setupRequests();
-      } catch (err) {
-        reject(err);
-      }
-    });
-    return result;
+    try {
+      validatePartnerParams(partner_params);
+      validateIdInfo(id_info);
+      data.partner_params.job_type = parseInt(partner_params.job_type, 10);
+      return setupRequests(data, options);
+    } catch (err) {
+      return Promise.reject(err);
+    }
   }
 }
 
