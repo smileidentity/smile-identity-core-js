1002365fe13b839e37adc2a86a197a1edcb6ee07
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-11-04 03:12:59 -0400

    address PR feedback

--- src/helpers.js
+++ src/helpers.js
@@ -14,7 +14,16 @@ const mapServerUri = (uriOrKey) => {
   if (String(uriOrKey) in sidServerMapping) {
     return sidServerMapping[uriOrKey];
   }
-  return uriOrKey;
+
+  if (typeof uriOrKey === 'string') {
+    // validates that it is a valid URI. Throws an error if not. Because server addresses do
+    // not have a protocol by default, we add a dummy protocol to the beginning of
+    // the URI to validate it.
+    // eslint-disable-next-line no-new
+    new URL(uriOrKey.startsWith('https://') ? uriOrKey : `https://${uriOrKey}`);
+    return uriOrKey;
+  }
+  throw new TypeError('Invalid URL');
 };
 
 module.exports = {
--- test/helpers.test.js
+++ test/helpers.test.js
@@ -3,20 +3,31 @@ const { mapServerUri } = require('../src/helpers');
 
 describe('helpers', () => {
   it('mapServerUri', () => {
-    const testCases = [
-      { input: null, expected: null },
-      { input: undefined, expected: undefined },
+    const validTestCases = [
       { input: 0, expected: 'testapi.smileidentity.com/v1' },
       { input: 1, expected: 'api.smileidentity.com/v1' },
       { input: '0', expected: 'testapi.smileidentity.com/v1' },
       { input: '1', expected: 'api.smileidentity.com/v1' },
       { input: 'testapi.smileidentity.com/v1', expected: 'testapi.smileidentity.com/v1' },
       { input: 'api.smileidentity.com/v1', expected: 'api.smileidentity.com/v1' },
-      { input: 'A', expected: 'A' },
+      { input: 'https://testapi.smileidentity.com/v1', expected: 'https://testapi.smileidentity.com/v1' },
+      { input: '10.0.0.1', expected: '10.0.0.1' },
     ];
 
-    testCases.forEach((testCase) => {
+    validTestCases.forEach((testCase) => {
       assert.equal(mapServerUri(testCase.input), testCase.expected);
+    });
+
+    const invalidTestCases = [
+      '',
+      ' ',
+      'https://',
+      null,
+      undefined,
+      2,
+    ];
+    invalidTestCases.forEach((testCase) => {
+      assert.throws(() => mapServerUri(testCase), TypeError('Invalid URL'), testCase);
     });
   });
 });
