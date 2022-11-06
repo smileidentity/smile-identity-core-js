841993dcac72f7c83d2cc6a2ca434ddd35f35daf
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-11-06 02:57:03 -0500

    refactor id api test

--- test/id-api.test.js
+++ test/id-api.test.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const keypair = require('keypair');
 const nock = require('nock');
 
@@ -8,108 +7,63 @@ const pair = keypair();
 
 describe('IDapi', () => {
   describe('#new', () => {
-    it('should instantiate and set the global variables', (done) => {
+    it('should instantiate and set the global variables', () => {
       const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      assert.equal(instance.partner_id, '001');
-      assert.equal(instance.api_key, Buffer.from(pair.public).toString('base64'));
-      assert.equal(instance.url, 'testapi.smileidentity.com/v1');
-      done();
+      expect(instance.partner_id).toEqual('001');
+      expect(instance.api_key).toEqual(Buffer.from(pair.public).toString('base64'));
+      expect(instance.url).toEqual('testapi.smileidentity.com/v1');
     });
   });
 
   describe('#submit_job', () => {
-    it('should ensure that the partner_params are present', (done) => {
+    it('should ensure that the partner_params are present', async () => {
       const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      instance.submit_job(null, {}).catch((err) => {
-        assert.equal(err.message, 'Please ensure that you send through partner params');
-        done();
+      await expect(instance.submit_job(null, {})).rejects.toThrow(new Error('Please ensure that you send through partner params'));
+    });
+
+    it('should ensure that the partner_params are an object', async () => {
+      const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
+      await expect(instance.submit_job('not partner params', {})).rejects.toThrow(new Error('Partner params needs to be an object'));
+    });
+
+    ['user_id', 'job_id', 'job_type'].forEach((key) => {
+      const partner_params = { user_id: '1', job_id: '1', job_type: 5 };
+      partner_params[key] = '';
+
+      it(`should ensure that partner_params contains ${key}`, async () => {
+        const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
+        await expect(instance.submit_job(partner_params, {}, {}, { return_job_status: true })).rejects.toThrow(new Error(`Please make sure that ${key} is included in the partner params`));
+      });
+
+      it(`should ensure that in partner_params, ${key} is not an empty string`, async () => {
+        const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
+        await expect(instance.submit_job(partner_params, {}, {}, { return_job_status: true })).rejects.toThrow(new Error(`Please make sure that ${key} is included in the partner params`));
       });
     });
 
-    it('should ensure that the partner_params are an object', (done) => {
+    it('should ensure that the id_info is an object', async () => {
       const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      instance.submit_job('not partner params', {}).catch((err) => {
-        assert.equal(err.message, 'Partner params needs to be an object');
-        done();
-      });
+      await expect(instance.submit_job({ user_id: '1', job_id: '1', job_type: 5 }, '')).rejects.toThrow(new Error('ID Info needs to be an object'));
     });
 
-    it('should ensure that the partner_params contain user_id, job_id and job_type', (done) => {
+    it('should ensure that the id_info object is not empty or nil', async () => {
       const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      ['user_id', 'job_id', 'job_type'].forEach((key) => {
-        const partner_params = {
-          user_id: '1',
-          job_id: '1',
-          job_type: 5,
-        };
-        delete partner_params[key];
-        instance.submit_job(partner_params, {}, {}, { return_job_status: true }).catch((err) => {
-          assert.equal(err.message, `Please make sure that ${key} is included in the partner params`);
-        });
-      });
-      done();
+      await expect(instance.submit_job({ user_id: '1', job_id: '1', job_type: 5 }, {})).rejects.toThrow(new Error('Please make sure that id_info not empty or nil'));
     });
 
-    it('should ensure that in partner_params, user_id, job_id, and job_type are not emptystrings', (done) => {
+    it('should ensure that the id_info object contains a valid id_number as a string', async () => {
       const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      ['user_id', 'job_id', 'job_type'].forEach((key) => {
-        const partner_params = {
-          user_id: '1',
-          job_id: '1',
-          job_type: 5,
-        };
-        partner_params[key] = '';
-        instance.submit_job(partner_params, {}, {}, { return_job_status: true }).catch((err) => {
-          assert.equal(err.message, `Please make sure that ${key} is included in the partner params`);
-        });
-      });
-      done();
+      await expect(instance.submit_job({ user_id: '1', job_id: '1', job_type: 5 }, { id_number: '' })).rejects.toThrow(new Error('Please provide an id_number in the id_info payload'));
     });
 
-    it('should ensure that the id_info is an object', (done) => {
+    it('should ensure that the the job id is set to 5', async () => {
       const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      instance.submit_job({ user_id: '1', job_id: '1', job_type: 5 }, '').catch((err) => {
-        assert.equal(err.message, 'ID Info needs to be an object');
-        done();
-      });
+      const partner_params = { user_id: '1', job_id: '1', job_type: 4 };
+      await expect(instance.submit_job(partner_params, null)).rejects.toThrow(new Error('Please ensure that you are setting your job_type to 5 to query ID Api'));
     });
 
-    it('should ensure that the id_info object is not empty or nil', (done) => {
-      const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      instance.submit_job({ user_id: '1', job_id: '1', job_type: 5 }, {}).catch((err) => {
-        assert.equal(err.message, 'Please make sure that id_info not empty or nil');
-        done();
-      });
-    });
-
-    it('should ensure that the id_info object contains a valid id_number as a string', (done) => {
-      const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      instance.submit_job({ user_id: '1', job_id: '1', job_type: 5 }, { id_number: '' }).catch((err) => {
-        assert.equal(err.message, 'Please provide an id_number in the id_info payload');
-        done();
-      });
-    });
-
-    it('should ensure that the the job id is set to 5', (done) => {
-      const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      const partner_params = {
-        user_id: '1',
-        job_id: '1',
-        job_type: 4,
-      };
-      instance.submit_job(partner_params, null).catch((err) => {
-        assert.equal(err.message, 'Please ensure that you are setting your job_type to 5 to query ID Api');
-        done();
-      });
-    });
-
-    it('should be able to send a job', (done) => {
-      const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      const partner_params = {
-        user_id: '1',
-        job_id: '1',
-        job_type: 5,
-      };
+    it('should be able to send a job', async () => {
+      const partner_params = { user_id: '1', job_id: '1', job_type: 5 };
       const id_info = {
         first_name: 'John',
         last_name: 'Doe',
@@ -146,40 +100,38 @@ describe('IDapi', () => {
         timestamp: 1570612182124,
       };
 
-      nock('https://testapi.smileidentity.com')
-        .post('/v1/id_verification', (body) => {
-          assert.equal(body.partner_id, '001');
-          assert.notEqual(body.sec_key, undefined);
-          assert.notEqual(body.timestamp, undefined);
-          assert.equal(body.partner_params.user_id, partner_params.user_id);
-          assert.equal(body.partner_params.job_id, partner_params.job_id);
-          assert.equal(body.partner_params.job_type, partner_params.job_type);
-          assert.equal(body.first_name, id_info.first_name);
-          assert.equal(body.last_name, id_info.last_name);
-          assert.equal(body.middle_name, id_info.middle_name);
-          assert.equal(body.country, id_info.country);
-          assert.equal(body.id_type, id_info.id_type);
-          assert.equal(body.id_number, id_info.id_number);
-          assert.equal(body.phone_number, id_info.phone_number);
-          return true;
-        })
-        .reply(200, IDApiResponse)
-        .isDone();
+      nock('https://testapi.smileidentity.com').post('/v1/id_verification', (body) => {
+        expect(body.partner_id).toEqual('001');
+        expect(body.sec_key).not.toEqual(undefined);
+        expect(body.timestamp).not.toEqual(undefined);
+        expect(body.partner_params.user_id).toEqual(partner_params.user_id);
+        expect(body.partner_params.job_id).toEqual(partner_params.job_id);
+        expect(body.partner_params.job_type).toEqual(partner_params.job_type);
+        expect(body.first_name).toEqual(id_info.first_name);
+        expect(body.last_name).toEqual(id_info.last_name);
+        expect(body.middle_name).toEqual(id_info.middle_name);
+        expect(body.country).toEqual(id_info.country);
+        expect(body.id_type).toEqual(id_info.id_type);
+        expect(body.id_number).toEqual(id_info.id_number);
+        expect(body.phone_number).toEqual(id_info.phone_number);
+        return true;
+      }).reply(200, IDApiResponse).isDone();
 
-      const promise = instance.submit_job(partner_params, id_info);
-      promise.then((resp) => {
-        assert.deepEqual(Object.keys(resp).sort(), ['JSONVersion', 'SmileJobID', 'PartnerParams', 'ResultType', 'ResultText', 'ResultCode', 'IsFinalResult', 'Actions', 'Country', 'IDType', 'IDNumber', 'ExpirationDate', 'FullName', 'DOB', 'Photo', 'sec_key', 'timestamp'].sort());
-        done();
-      });
+      const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
+
+      const response = await instance.submit_job(partner_params, id_info);
+      expect(Object.keys(response).sort()).toEqual([
+        'JSONVersion', 'SmileJobID', 'PartnerParams', 'ResultType',
+        'ResultText', 'ResultCode', 'IsFinalResult', 'Actions',
+        'Country', 'IDType', 'IDNumber', 'ExpirationDate',
+        'FullName', 'DOB', 'Photo', 'sec_key', 'timestamp',
+      ].sort());
+      expect.assertions(14);
     });
 
-    it('should raise an error when a network call fails', (done) => {
+    it('should raise an error when a network call fails', async () => {
       const instance = new IDApi('001', Buffer.from(pair.public).toString('base64'), 0);
-      const partner_params = {
-        user_id: '1',
-        job_id: '1',
-        job_type: 5,
-      };
+      const partner_params = { user_id: '1', job_id: '1', job_type: 5 };
       const id_info = {
         first_name: 'John',
         last_name: 'Doe',
@@ -190,32 +142,28 @@ describe('IDapi', () => {
         phone_number: '0726789065',
       };
 
-      nock('https://testapi.smileidentity.com')
-        .post('/v1/id_verification')
-        .replyWithError(400, {
-          code: '2204',
-          error: 'unauthorized',
-        })
-        .isDone();
+      nock('https://testapi.smileidentity.com').post('/v1/id_verification').replyWithError(400, {
+        code: '2204',
+        error: 'unauthorized',
+      }).isDone();
 
-      instance.submit_job(partner_params, id_info).then(() => {
-        assert.equal(false);
-      }).catch((err) => {
-        // todo: figure out how to get nook to act like an error response would in real life
-        // err.message in this case should be '2204:unauthorized'
-        assert.equal(err.message, undefined);
-      });
-
-      done();
+      let response;
+      let error;
+      try {
+        response = await instance.submit_job(partner_params, id_info);
+      } catch (e) {
+        error = e;
+      }
+      // todo: figure out how to get nook to act like an error response would in real life
+      // err.message in this case should be '2204:unauthorized'
+      expect(error).toEqual('undefined:undefined');
+      expect(response).toEqual(undefined);
+      expect.assertions(2);
     });
 
-    it('should use the signature instead of sec_key when provided an optional parameter', (done) => {
+    it('should use the signature instead of sec_key when provided an optional parameter', async () => {
       const instance = new IDApi('001', '1234', 0);
-      const partner_params = {
-        user_id: '1',
-        job_id: '1',
-        job_type: 5,
-      };
+      const partner_params = { user_id: '1', job_id: '1', job_type: 5 };
       const id_info = {
         first_name: 'John',
         last_name: 'Doe',
@@ -252,31 +200,33 @@ describe('IDapi', () => {
         timestamp: 1570612182124,
       };
 
-      nock('https://testapi.smileidentity.com')
-        .post('/v1/id_verification', (body) => {
-          assert.equal(body.partner_id, '001');
-          assert.notEqual(body.signature, undefined);
-          assert.notEqual(body.timestamp, undefined);
-          assert.equal(body.partner_params.user_id, partner_params.user_id);
-          assert.equal(body.partner_params.job_id, partner_params.job_id);
-          assert.equal(body.partner_params.job_type, partner_params.job_type);
-          assert.equal(body.first_name, id_info.first_name);
-          assert.equal(body.last_name, id_info.last_name);
-          assert.equal(body.middle_name, id_info.middle_name);
-          assert.equal(body.country, id_info.country);
-          assert.equal(body.id_type, id_info.id_type);
-          assert.equal(body.id_number, id_info.id_number);
-          assert.equal(body.phone_number, id_info.phone_number);
-          return true;
-        })
-        .reply(200, IDApiResponse)
-        .isDone();
+      nock('https://testapi.smileidentity.com').post('/v1/id_verification', (body) => {
+        expect(body.partner_id).toEqual('001');
+        expect(body.signature).not.toEqual(undefined);
+        expect(body.timestamp).not.toEqual(undefined);
+        expect(body.partner_params.user_id).toEqual(partner_params.user_id);
+        expect(body.partner_params.job_id).toEqual(partner_params.job_id);
+        expect(body.partner_params.job_type).toEqual(partner_params.job_type);
+        expect(body.first_name).toEqual(id_info.first_name);
+        expect(body.last_name).toEqual(id_info.last_name);
+        expect(body.middle_name).toEqual(id_info.middle_name);
+        expect(body.country).toEqual(id_info.country);
+        expect(body.id_type).toEqual(id_info.id_type);
+        expect(body.id_number).toEqual(id_info.id_number);
+        expect(body.phone_number).toEqual(id_info.phone_number);
+        return true;
+      }).reply(200, IDApiResponse).isDone();
 
-      const promise = instance.submit_job(partner_params, id_info, { signature: true });
-      promise.then((resp) => {
-        assert.deepEqual(Object.keys(resp).sort(), ['JSONVersion', 'SmileJobID', 'PartnerParams', 'ResultType', 'ResultText', 'ResultCode', 'IsFinalResult', 'Actions', 'Country', 'IDType', 'IDNumber', 'ExpirationDate', 'FullName', 'DOB', 'Photo', 'signature', 'timestamp'].sort());
-        done();
-      });
+      const response = await instance.submit_job(partner_params, id_info, { signature: true });
+
+      expect(Object.keys(response).sort()).toEqual([
+        'JSONVersion', 'SmileJobID', 'PartnerParams', 'ResultType',
+        'ResultText', 'ResultCode', 'IsFinalResult', 'Actions',
+        'Country', 'IDType', 'IDNumber', 'ExpirationDate', 'FullName',
+        'DOB', 'Photo', 'signature', 'timestamp',
+      ].sort());
+
+      expect.assertions(14);
     });
   });
 });
