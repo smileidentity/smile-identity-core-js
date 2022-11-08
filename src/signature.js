5cd24351622a1094e2e328978e6542c2d4e01988
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-11-08 01:18:49 -0500

    lint

--- src/signature.js
+++ src/signature.js
@@ -1,21 +1,32 @@
 const crypto = require('crypto');
 
+/**
+ *
+ * @param {string} partnerID - Smile partner ID. This is a unique identifier
+ * for your Smile account.
+ * @param {string} apiKey - Smile API Key. Found in the Smile Dashboard.
+ * @param {string|number} timestamp - ISO 8601 timestamp or unix timestamp.
+ * @returns {string} the calculated signature.
+ */
 const generate_signature = (partnerID, apiKey, timestamp) => {
-  if (typeof timestamp === 'number') {
-    timestamp = new Date(timestamp).toISOString();
-  }
+  const isoTimestamp = typeof timestamp === 'number' ? new Date(timestamp).toISOString() : timestamp;
+  // validates that the timestamp is a valid ISO 8601 timestamp.
+  new Date(isoTimestamp).toISOString(); // eslint-disable-line no-new
   const hmac = crypto.createHmac('sha256', apiKey);
-  hmac.update(timestamp, 'utf8');
+  hmac.update(isoTimestamp, 'utf8');
   hmac.update(partnerID, 'utf8');
   hmac.update('sid_request', 'utf8');
   return hmac.digest().toString('base64');
 };
 
+/* A class to generate signatures for a given partner ID and API key. */
 class Signature {
   /**
+   * Instantiates a new Signature object.
    *
-   * @param {string} partnerID
-   * @param {string} apiKey
+   * @param {string} partnerID - Smile Partner ID. This is a unique identifier
+   * for your Smile account.
+   * @param {string} apiKey - Smile API Key. Found in the Smile Dashboard.
    */
   constructor(partnerID, apiKey) {
     this.partnerID = partnerID;
@@ -23,9 +34,15 @@ class Signature {
   }
 
   /**
+   * Generates a signature for a given timestamp.
    *
-   * @param {string|number|undefined} timestamp
-   * @returns
+   * @param {string|number|undefined} timestamp - A valid ISO 8601 timestamp or unix time in
+   * milliseconds. If undefined, the current time will be used.
+   * @returns {{
+   *  signature: string,
+   *  timestamp: number,
+   * }} - An object containing the signature and timestamp.
+   * @throws {Error} - If the timestamp is invalid.
    */
   generate_signature(timestamp = new Date().toISOString()) {
     return {
--- test/signature.test.js
+++ test/signature.test.js
@@ -17,12 +17,60 @@ describe('Signature', () => {
   });
 
   describe('#generate_signature', () => {
+    it('should generate a signature with a default timestamp', () => {
+      const mockApiKey = Buffer.from(pair.public).toString('base64');
+      const timestamp = new Date().getTime();
+      const result = new Signature('001', mockApiKey).generate_signature(timestamp);
+
+      assert.equal(typeof result.signature, 'string');
+      assert.equal(typeof result.timestamp, 'number');
+      assert.ok(result.timestamp <= new Date().getTime());
+    });
+
+    it('should generate a signature for valid timestamps', (done) => {
+      const validTimestampFormats = [
+        '2018-01-01T00:00:00.000Z',
+        '2018-01-01T00:00:00.000+00:00',
+        '2018-01-01T00:00:00.000+0000',
+        new Date().toISOString(),
+        Date.now(),
+        new Date().getTime(),
+      ];
+
+      validTimestampFormats.forEach((timestamp) => {
+        const mockApiKey = Buffer.from(pair.public).toString('base64');
+        const result = new Signature('001', mockApiKey).generate_signature(timestamp);
+        const isoTimestamp = typeof timestamp === 'number' ? new Date(timestamp).toISOString() : timestamp;
+        const hmac = crypto.createHmac('sha256', mockApiKey);
+        hmac.update(isoTimestamp, 'utf8').update('001', 'utf8').update('sid_request', 'utf8');
+        const output = hmac.digest().toString('base64');
+        assert.equal(output, result.signature);
+        assert.equal(timestamp, result.timestamp);
+      });
+      done();
+    });
+
+    it('should throw an error for invalid timestamps', (done) => {
+      [NaN, '', '2018-00-01T00:00:00.000'].forEach((timestamp) => {
+        const mockApiKey = Buffer.from(pair.public).toString('base64');
+        let result;
+        let error;
+
+        try {
+          result = new Signature('001', mockApiKey).generate_signature(timestamp);
+        } catch (e) {
+          error = e;
+        }
+        assert.equal(result, undefined);
+        assert.equal(error.message, 'Invalid time value');
+      });
+      done();
+    });
+
     it('should calculate a signature and use a timestamp if provided one', (done) => {
       const timestamp = new Date().toISOString();
       const hmac = crypto.createHmac('sha256', '1234');
-      hmac.update(timestamp, 'utf8');
-      hmac.update('002', 'utf8');
-      hmac.update('sid_request', 'utf8');
+      hmac.update(timestamp, 'utf8').update('002', 'utf8').update('sid_request', 'utf8');
       const output = hmac.digest().toString('base64');
       const result = new Signature('002', '1234').generate_signature(timestamp);
       assert.equal(output, result.signature);
@@ -32,12 +80,10 @@ describe('Signature', () => {
   });
 
   describe('#confirm_signature', () => {
-    it('should confirm an incoming signaute', (done) => {
+    it('should confirm an incoming signature', (done) => {
       const timestamp = new Date().toISOString();
       const hmac = crypto.createHmac('sha256', '1234');
-      hmac.update(timestamp, 'utf8');
-      hmac.update('002', 'utf8');
-      hmac.update('sid_request', 'utf8');
+      hmac.update(timestamp, 'utf8').update('002', 'utf8').update('sid_request', 'utf8');
       const output = hmac.digest().toString('base64');
       assert.equal(true, new Signature('002', '1234').confirm_signature(timestamp, output));
       done();
