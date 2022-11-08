b91d1504ddcfadc9935ce87b31ff29899af7ce98
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-11-07 20:26:43 -0500

    start to refactor signature class

--- src/signature.js
+++ src/signature.js
@@ -1,22 +1,41 @@
 const crypto = require('crypto');
 
+const generate_signature = (partnerID, apiKey, timestamp) => {
+  if (typeof timestamp === 'number') {
+    timestamp = new Date(timestamp).toISOString();
+  }
+  const hmac = crypto.createHmac('sha256', apiKey);
+  hmac.update(timestamp, 'utf8');
+  hmac.update(partnerID, 'utf8');
+  hmac.update('sid_request', 'utf8');
+  return hmac.digest().toString('base64');
+};
+
 class Signature {
+  /**
+   *
+   * @param {string} partnerID
+   * @param {string} apiKey
+   */
   constructor(partnerID, apiKey) {
     this.partnerID = partnerID;
     this.apiKey = apiKey;
   }
 
+  /**
+   *
+   * @param {string|number|undefined} timestamp
+   * @returns
+   */
   generate_signature(timestamp = new Date().toISOString()) {
-    const hmac = crypto.createHmac('sha256', this.apiKey);
-    hmac.update(timestamp, 'utf8');
-    hmac.update(this.partnerID, 'utf8');
-    hmac.update('sid_request', 'utf8');
-    const output = hmac.digest().toString('base64');
-    return { signature: output, timestamp };
+    return {
+      signature: generate_signature(this.partnerID, this.apiKey, timestamp),
+      timestamp,
+    };
   }
 
   confirm_signature(timestamp, signature) {
-    return signature === this.generate_signature(timestamp).signature;
+    return generate_signature(this.partnerID, this.apiKey, timestamp) === signature;
   }
 }
 
