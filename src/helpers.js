cf3e3b8118db9ee95505bb31c3ae357b08277af7
Michael <michael.l.dangelo@gmail.com>
2022-11-09 15:37:16 -0500

    refactor id api class (#43)
    * refactor id api class
    
    * update changelog
    
    * move cast
    
    * refactor function
    
    * move function
    
    * abstract function
    
    * add unit test
    
    * add comment
    
    * fix changelog

--- src/helpers.js
+++ src/helpers.js
@@ -25,7 +25,34 @@ const sdkVersionInfo = {
   source_sdk_version: packageJson.version,
 };
 
+/**
+ * Validates that partner params contains required fields.
+ *
+ * @param {object} partnerParams - required parameters for each job.
+ * @param {string} partnerParams.user_id - your unique identifier for the user.
+ * @param {string} partnerParams.job_id - your unique identifier for the job.
+ * @param {string|number} partnerParams.job_type - type of job.
+ * @throws {Error} if partnerParams is not an object or is missing required keys.
+ * @returns {void}
+ */
+const validatePartnerParams = (partnerParams) => {
+  if (!partnerParams) {
+    throw new Error('Please ensure that you send through partner params');
+  }
+
+  if (typeof partnerParams !== 'object') {
+    throw new Error('Partner params needs to be an object');
+  }
+
+  ['user_id', 'job_id', 'job_type'].forEach((key) => {
+    if (!partnerParams[key]) {
+      throw new Error(`Please make sure that ${key} is included in the partner params`);
+    }
+  });
+};
+
 module.exports = {
   mapServerUri,
   sdkVersionInfo,
+  validatePartnerParams,
 };
