1bfedb96faf2294bcb4d1002acb983ca9eef6535
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-11-12 18:47:05 -0500

    convert utilities to use axios

--- test/utilities.test.js
+++ test/utilities.test.js
@@ -5,10 +5,23 @@ const nock = require('nock');
 const { Utilities, Signature, JOB_TYPE } = require('..');
 
 const pair = keypair();
+const mockApiKey = Buffer.from(pair.public).toString('base64');
 
 describe('Utilities', () => {
+  before(() => {
+    nock.disableNetConnect();
+  });
+
+  after(() => {
+    nock.enableNetConnect();
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
   describe('#get_job_status', () => {
-    it('should be able to check job_status successfully', (done) => {
+    it('should be able to check job_status successfully', async () => {
       const partner_params = {
         user_id: '1',
         job_id: '1',
@@ -21,7 +34,7 @@ describe('Utilities', () => {
       };
 
       const timestamp = new Date().toISOString();
-      const { signature } = new Signature('001', '1234').generate_signature(timestamp);
+      const { signature } = new Signature('001', mockApiKey).generate_signature(timestamp);
 
       const jobStatusResponse = {
         job_success: true,
@@ -33,31 +46,28 @@ describe('Utilities', () => {
         timestamp,
         signature,
       };
-      nock('https://testapi.smileidentity.com')
-        .post('/v1/job_status', (body) => {
-          assert.equal(body.job_id, partner_params.job_id);
-          assert.equal(body.user_id, partner_params.user_id);
-          assert.notEqual(body.timestamp, undefined);
-          assert.notEqual(body.signature, undefined);
-          assert.equal(body.image_links, true);
-          assert.equal(body.history, true);
-          return true;
-        })
-        .reply(200, jobStatusResponse)
-        .isDone();
-      new Utilities('001', '1234', 0)
-        .get_job_status(partner_params.user_id, partner_params.job_id, options)
-        .then((job_status) => {
-          assert.equal(job_status.signature, jobStatusResponse.signature);
-          assert.equal(job_status.job_complete, true);
-          done();
-        }).catch((err) => {
-          assert.equal(null, err);
-          console.error(err);
-        });
+      const scope = nock('https://testapi.smileidentity.com').post('/v1/job_status', (body) => {
+        assert.equal(body.job_id, partner_params.job_id);
+        assert.equal(body.user_id, partner_params.user_id);
+        assert.notEqual(body.timestamp, undefined);
+        assert.notEqual(body.signature, undefined);
+        assert.equal(body.image_links, true);
+        assert.equal(body.history, true);
+        return true;
+      }).reply(200, jobStatusResponse);
+      const utilities = new Utilities('001', mockApiKey, 0);
+      const jobStatus = await utilities.get_job_status(
+        partner_params.user_id,
+        partner_params.job_id,
+        options,
+      );
+      assert.equal(jobStatus.signature, jobStatusResponse.signature);
+      assert.equal(jobStatus.job_success, true);
+      assert.equal(jobStatus.job_complete, true);
+      assert.ok(scope.isDone());
     });
 
-    it('should raise an error if one occurs', (done) => {
+    it('should raise an error if one occurs', async () => {
       const partner_params = {
         user_id: '1',
         job_id: '1',
@@ -68,29 +78,35 @@ describe('Utilities', () => {
         return_history: true,
       };
 
-      nock('https://testapi.smileidentity.com')
-        .post('/v1/job_status', (body) => {
-          assert.equal(body.job_id, partner_params.job_id);
-          assert.equal(body.user_id, partner_params.user_id);
-          assert.notEqual(body.timestamp, undefined);
-          assert.notEqual(body.signature, undefined);
-          assert.equal(body.image_links, true);
-          assert.equal(body.history, true);
-          return true;
-        })
-        .replyWithError(400, {
-          code: '2204',
-          error: 'unauthorized',
-        })
-        .isDone();
-      new Utilities('001', Buffer.from(pair.public).toString('base64'), 0)
-        .get_job_status(partner_params.user_id, partner_params.job_id, options)
-        .then((job_status) => {
-          assert.equal(null, job_status);
-        }).catch((err) => {
-          assert.equal(err.message, 'Error: 400');
-          done();
-        });
+      const scope = nock('https://testapi.smileidentity.com').post('/v1/job_status', (body) => {
+        assert.equal(body.job_id, partner_params.job_id);
+        assert.equal(body.user_id, partner_params.user_id);
+        assert.notEqual(body.timestamp, undefined);
+        assert.notEqual(body.signature, undefined);
+        assert.equal(body.image_links, true);
+        assert.equal(body.history, true);
+        return true;
+      }).replyWithError(400, {
+        code: '2204',
+        error: 'unauthorized',
+      });
+      const utilities = new Utilities('001', mockApiKey, 0);
+
+      let jobStatus;
+      let error;
+      try {
+        jobStatus = await utilities.get_job_status(
+          partner_params.user_id,
+          partner_params.job_id,
+          options,
+        );
+      } catch (err) {
+        error = err;
+      }
+      assert.equal(jobStatus, undefined);
+      assert.notEqual(error, undefined);
+      assert.equal(error.cause.message, 400);
+      assert.ok(scope.isDone());
     });
   });
 });
