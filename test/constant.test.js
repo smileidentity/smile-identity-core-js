65c7060882f5b8664ba6dc94ef2be33c971837fd
Barnabas Nsoh <banasco@gmail.com>
2022-12-13 14:19:15 +0000

    fix lint

--- test/constant.test.js
+++ test/constant.test.js
@@ -1,17 +1,15 @@
 const { JOB_TYPE } = require('..');
 
 describe('Constant', () => {
-  it('should have correct jobtyps', ()=>{
-     expect(JOB_TYPE.BIOMETRIC_KYC).toEqual(1);
-     expect(JOB_TYPE.SMART_SELFIE_AUTHENTICATION).toEqual(2);
-     expect(JOB_TYPE.SMART_SELFIE_REGISTRATION).toEqual(4);
-     expect(JOB_TYPE.BASIC_KYC).toEqual(5);
-     expect(JOB_TYPE.ENHANCED_KYC).toEqual(5);
-     expect(JOB_TYPE.DOCUMENT_VERIFICATION).toEqual(6);
-     expect(JOB_TYPE.BUSINESS_VERIFICATION).toEqual(7);
-     expect(JOB_TYPE.UPDATE_PHOTO).toEqual(8);
-     expect(JOB_TYPE.COMPARE_USER_INFO).toEqual(9);
-  })
-
-  
-})
+  it('should have correct jobtyps', () => {
+    expect(JOB_TYPE.BIOMETRIC_KYC).toEqual(1);
+    expect(JOB_TYPE.SMART_SELFIE_AUTHENTICATION).toEqual(2);
+    expect(JOB_TYPE.SMART_SELFIE_REGISTRATION).toEqual(4);
+    expect(JOB_TYPE.BASIC_KYC).toEqual(5);
+    expect(JOB_TYPE.ENHANCED_KYC).toEqual(5);
+    expect(JOB_TYPE.DOCUMENT_VERIFICATION).toEqual(6);
+    expect(JOB_TYPE.BUSINESS_VERIFICATION).toEqual(7);
+    expect(JOB_TYPE.UPDATE_PHOTO).toEqual(8);
+    expect(JOB_TYPE.COMPARE_USER_INFO).toEqual(9);
+  });
+});
