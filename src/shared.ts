1215836f10adc8211bb52d4184e8105f07edec1d
Barnabas Nsoh <banasco@gmail.com>
2022-12-19 13:09:52 +0000

    convert utilities to ts

--- src/shared.ts
+++ src/shared.ts
@@ -1,7 +1,16 @@
+interface OptionsParam {
+  return_history?: boolean;
+  return_images?: boolean;
+  return_image_links?: boolean;
+  return_job_status?: boolean;
+  use_enrolled_image?: boolean;
+  optional_callback?: string
+}
+
 interface PartnerParams {
   user_id: string | number;
   job_id: string | number;
   job_type: number;
 }
 
-export { type PartnerParams};
+export { type PartnerParams, type OptionsParam};
