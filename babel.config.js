35adea6525fb796bf23dfb9912ec62c5cc814a70
Michael D'Angelo <michael.l.dangelo@gmail.com>
2022-12-20 11:54:13 -0500

    use babel to target earlier node versions

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+// strips leading >=v
+const nodeVersionNumber = packageJson.engines.node.replace(/^>=v/, '');
+
+module.exports = {
+  presets: [['@babel/preset-env', {targets: {node: nodeVersionNumber }}]],
+};
