9d07a2fda123626c9f3c53ec071dc12987d6cdc9
Barnabas Nsoh <banasco@gmail.com>
2022-12-21 11:47:46 +0000

    change document verfication example to ts

--- examples/document_verification.ts
+++ examples/document_verification.ts
@@ -4,7 +4,8 @@
  */
 
 // Change to smile-identity-core if you're using this snippet in your project.
-const { WebApi } = require('..');
+/* eslint-disable import/no-relative-packages */
+import { WebApi } from '../dist';
 
 // Initialize
 // login to the Smile Identity portal to view your partner id.
