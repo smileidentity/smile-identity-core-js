a40e313b62ba4de22a3ee15f97e83226b8fdfeef
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-01-31 16:28:04 -0500

    revert babel change

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'),
+);
+// strips leading >=v
+const nodeVersionNumber = packageJson.engines.node.replace(/^>=v/, '');
+
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: 'node 14.0' }],
+    ['@babel/preset-env', { targets: { node: nodeVersionNumber } }],
     '@babel/preset-typescript',
   ],
 };
